e6e7d729e18cd23d7d77de44051139ef3fc72eba
jannik-reaZn <jannik.harm@gmail.com>
2026-04-30 18:37:09 +0000

    Created api route and tag enum

---
+++ backend/app/presentation/api/routes.py
@@ -0,0 +1,5 @@
+from enum import StrEnum
+
+
+class ApiRoute(StrEnum):
+    TODOS = "/todos"
--- backend/app/presentation/api/todos/todo_routes.py
+++ backend/app/presentation/api/todos/todo_routes.py
@@ -5,14 +5,18 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status
 
 from app.todos.application.create_todo import CreateTodoUseCase
+from app.presentation.api.routes import ApiRoute
+from app.presentation.api.tags import ApiTag
 from app.presentation.api.todos.dependencies import get_create_todo_use_case
 from app.presentation.api.todos.create_todo_request import CreateTodoRequest
 from app.presentation.api.todos.todo_response import TodoResponse
 
-router = APIRouter()
+router = APIRouter(tags=[ApiTag.TODOS])
 
 
-@router.post("/todos", response_model=TodoResponse, status_code=status.HTTP_201_CREATED)
+@router.post(
+    ApiRoute.TODOS, response_model=TodoResponse, status_code=status.HTTP_201_CREATED
+)
 def create_todo(
     request: CreateTodoRequest,
     create_todo_use_case: Annotated[
